2766a151c9f0e44730fcdfa73c88bce47113fc27
adekmo <monangbahana@gmail.com>
2023-03-13 22:29:58 +0700

    home & piechart

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { useList } from '@pankod/refine-core';
+import {
+    PieChart,
+    PropertyReferrals,
+    TotalRevenue,
+    PropertyCard,
+    TopAgent
+} from 'components'
+import { Box, Stack, Typography } from '@pankod/refine-mui';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box>
+            <Typography fontSize={25} fontWeight={700} color="#11142D">
+                Dashboard
+            </Typography>
+
+            <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+                <PieChart 
+                    title="Properties for sale"
+                    value={684}
+                    series={[75, 25]}
+                    colors={['#275be8', '#c4e8ef']}
+                />
+                <PieChart 
+                    title="Properties for rent"
+                    value={550}
+                    series={[60, 40]}
+                    colors={['#275be8', '#c4e8ef']}
+                />
+                <PieChart 
+                    title="Total Customers"
+                    value={5684}
+                    series={[75, 25]}
+                    colors={['#275be8', '#c4e8ef']}
+                />
+                <PieChart 
+                    title="Total City"
+                    value={555}
+                    series={[75, 25]}
+                    colors={['#275be8', '#c4e8ef']}
+                />
+            </Box>
+
+            <Stack></Stack>
+        </Box>
     )
 }
 
